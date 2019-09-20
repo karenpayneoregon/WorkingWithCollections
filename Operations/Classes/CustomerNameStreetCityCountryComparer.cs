8e844a9eb9255df9bc8c22d989d1e03823ba296d
Karen Payne <paynekaren@comcast.net>
2019-09-19 17:37:38 -0700

    Refactor

--- Operations/Classes/CustomerNameStreetCityCountryComparer.cs
+++ Operations/Classes/CustomerNameStreetCityCountryComparer.cs
@@ -6,10 +6,12 @@ namespace Operations.Classes
     {
         public bool Equals(Customer customer1, Customer customer2)
         {
-            return customer1.CompanyName == customer2.CompanyName &&
-                   customer1.Street == customer2.Street &&
-                   customer1.City == customer2.City &&
-                   customer1.CountryIdentfier == customer2.CountryIdentfier;
+            return customer2 != null && 
+                   (customer1 != null && 
+                    (customer1.CompanyName == customer2.CompanyName &&
+                     customer1.Street == customer2.Street &&
+                     customer1.City == customer2.City &&
+                     customer1.CountryIdentfier == customer2.CountryIdentfier));
         }
 
         public int GetHashCode(Customer obj)
